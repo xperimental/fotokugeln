45c649e48120c8ad62a603b99716a283b3d8d195
Robert <xperimental@solidproject.de>
2010-12-28 01:33:57 +0100

    Created handler for KML download.
     - Uses templating engine to create KML file.

--- fotokugeln.py
+++ fotokugeln.py
@@ -6,6 +6,7 @@ from handlers.process import ProcessHandler
 from handlers.download import RawPanoHandler, ThumbnailHandler, TileHandler
 from handlers.index import IndexHandler
 from handlers.upload import UploadHandler
+from handlers.kml import KmlDownloadHandler
 
 fotokugeln_app = webapp.WSGIApplication(
         [
@@ -15,6 +16,7 @@ fotokugeln_app = webapp.WSGIApplication(
                 ('/pano/process/(.+)', ProcessHandler),
                 ('/pano/thumbnail/(.+)', ThumbnailHandler),
                 ('/pano/tile/(.+)/(\d+)/(\d+)/(\d+)', TileHandler),
+                ('/pano/view/(.+)', KmlDownloadHandler),
                 ('/', IndexHandler)
         ], debug=True)
 
